3068c49c1e69e324c6b80410c11827504af1783c
root <root@puppet.codespaces.io>
2017-10-28 19:57:31 +0000

    node 3

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
     
     notify { "**** NODE1 BLOCK ****":    }
     
@@ -16,38 +16,5 @@ node 'node1' {
         checksum_value  => '82a88f711b94fa7564da432f2c52643f'
     }
     
-    include base
 }
 
-node 'node2' {
-    
-    notify { "**** NODE2 BLOCK ****":    }
-    
-    
-    class { '::tomcat':
-        xms             => '50m',
-        xmx             => '320m',
-        user            => 'tomcat',
-        group           => 'tomcat',
-        service_state   => running
-        
-    }
-    
-    tomcat::deploy { "sysfoo":
-        deploy_url          => 'https://2-108525006-gh.circle-artifacts.com/0/tmp/circle-artifacts.fbcEDtP/sysfoo.war',
-        checksum            => 'md5',
-        checksum_value      => '82a88f711b94fa7564da432f2c52643f'
-    }
-    
-    include base
-}
-
-
-node default {
-
-    notify { "checkpoint_1":
-    
-        message     => " ********** DEFAULT BLOCK ********** "
-        
-    }
-}
